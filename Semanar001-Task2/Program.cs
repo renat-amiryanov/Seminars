79c687cd4e798b40a3107b9e161192064aea6e2e
renat-amiryanov <rfamiryanov@gmail.com>
2022-06-27 14:40:44 +0300

    Форматирование кода.

--- Semanar001-Task2/Program.cs
+++ Semanar001-Task2/Program.cs
@@ -4,11 +4,15 @@ a = 5, b = 7 -> max = 7*/
 
 Console.WriteLine("Программа, которая на вход принимает два числа\n"
 + "и выдает, какое число большее, а какое меньшее.");
+
 Console.Write("Введите число a: ");
 int a = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Введите число b: ");
 int b = Convert.ToInt32(Console.ReadLine());
+
 int min = a, max = b;
+
 if (a > max)
 {
     int tmp = max;
