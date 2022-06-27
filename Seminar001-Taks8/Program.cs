754c092b0cd7230a5e7ddf3f33f263ab5fa084c4
renat-amiryanov <rfamiryanov@gmail.com>
2022-06-27 17:42:12 +0300

    Практическая работа. Семинар 1. Задача 8.

---
+++ Seminar001-Taks8/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Задача 8. Написать прорамму, которая на вход принимает число (N),
+    а на выходе показывает все четные числа от 1 до N
+*/
+
+Console.WriteLine("Программа, которая на вход принимает число (N),\n"
+   + "а на выходе показывает все четные числа от 1 до N");
+
+Console.Write("Введите число N: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int counter = 1;
+
+string message = n + " -> ";
+
+while (counter <= n)
+{
+    if (counter % 2 == 0)
+    {
+        message = counter != n ? message + counter + ", " : message + counter;
+    }
+    counter++;
+}
+
+Console.WriteLine(message);
