41bdbf823e4ddbb37cb38b5e60abe0afcb8c37af
renat-amiryanov <rfamiryanov@gmail.com>
2022-06-27 15:02:45 +0300

    Практическая работа. Семинар 1. Задача 4.

--- Seminar001-Task4/Program.cs
+++ Seminar001-Task4/Program.cs
@@ -3,4 +3,18 @@
 и выдает максимальное из этих чисел.
 */
 
+Console.WriteLine("Программа, которая на вход принимает три числа\n" +
+"и выдает максимальное из этих чисел.");
+
+Console.Write("Введите первое число:\t");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:\t");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:\t");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("{0} {1} {2} -> {3}", a, b, c, max);
 
