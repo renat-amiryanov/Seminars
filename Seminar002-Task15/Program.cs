dece5723f75a1b62b940e72b217279aab34685b7
renat-amiryanov <rfamiryanov@gmail.com>
2022-06-28 14:53:45 +0300

    Практическая работа. Семинар 2. Задача 15

--- Seminar002-Task15/Program.cs
+++ Seminar002-Task15/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Задача 15. Напишите программу, котарая принимает на вход цифру,
+    обозначающую день недели, и проверяет, является ли этот день выходным
+*/
+Console.Write("Введите цифру обоначающую день недели: ");
+int dayNumberOfWeek = Convert.ToInt32(Console.ReadLine());
+string checkIsHolyday(int dayNumber)
+{
+    Console.Write(dayNumber + " -> ");
+
+    switch (dayNumber)
+    {
+        case 1:
+            return "Нет";
+            break;
+        case 2:
+            return "Нет";
+            break;
+        case 3:
+            return "Нет";
+            break;
+        case 4:
+            return "Нет";
+            break;
+        case 5:
+            return "Нет";
+            break;
+        case 6:
+            return "Да";
+            break;
+        case 7:
+            return "Да";
+            break;
+        default:
+            return "Некорректный ввод. Такого номера дня не существет.";
+            break;
+    }
+}
+
+Console.WriteLine(checkIsHolyday(dayNumberOfWeek));
