516a3e0e0709c60a5e6c5c497859e168515c17a2
renat-amiryanov <rfamiryanov@gmail.com>
2022-07-03 18:20:52 +0300

    Практическая работа. Семинар 2. Задача 13

--- Seminar002-Task13/Program.cs
+++ Seminar002-Task13/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+//метод который возвращает длину заданного числа
+int getLength(int n)
+{
+    int count = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        count++;
+    }
+    return count;
+}
+
+// метод в поиска и вывода третьей цифры
+void findAndPrint3rdDigit(int number)
+{
+    Console.Write("{0} -> ", number);
+
+    //проверяем длину заданного числа, если меньш 3, то выводим сообщение
+    if (getLength(number) < 3)
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else
+    {
+        int counter = getLength(number) - 3;
+        // Console.WriteLine(counter);
+        while (counter != 0)
+        {
+            number = number / 10;
+            counter--;
+        }
+
+        int thirdDigit = (number % 100) % 10;
+        Console.WriteLine(thirdDigit);
+    }
+
+}
+
+findAndPrint3rdDigit(645);
+findAndPrint3rdDigit(78);
+findAndPrint3rdDigit(32679);
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+findAndPrint3rdDigit(number);
