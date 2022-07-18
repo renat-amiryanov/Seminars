532aabf5c08975a945d3192205ba7eef3c883171
renat-amiryanov <rfamiryanov@gmail.com>
2022-07-18 08:58:24 +0300

    Практическое задание. Семинар 5. Задача 34.

--- Seminar005-Task34/Program.cs
+++ Seminar005-Task34/Program.cs
@@ -1,10 +1,17 @@
 ﻿/*
     Задача 34. Задайте массив заполненный случайными положительными трехзначнымичислами.
-    Напишите программу, которая покажет количество четныхчисле в массиве.
+    Напишите программу, которая покажет количество четных чисел в массиве.
 
     [345,897,568,234] -> 2
 
-    n % 2 == 0 
+    1. Метод CreateAndFillArray() создает,заполняет, возвращает массив целых  чисел.
+        - size, параметр который определеяет размер массива, по умлочанию 4
+        - min и max параметры которые задают левую и правую границу генерируемого случайного числа
+        по умлочанию min = 100, max = 1000
+
+    2. Метод GetCountOfEvenNumbers() ведет подсчет и возвращает количество четных  целых чисел в массиве котрый передается на вход в метода
+
+    3. Метод CheckingSolution() метод проверки работы метода GetCountOfEvenNumbers().
 
  */
 
@@ -19,28 +26,27 @@ int[] CreateAndFillArray(int size = 4, int min = 100, int max = 1000)
     return returnArray;
 }
 
-
-
 int GetCountOfEvenNumbers(int[] input)
 {
 
     int count = 0;
     for (int i = 0; i < input.Length; i++)
     {
-        if (input[i] % 2 == 0) count++  ;
+        if (input[i] % 2 == 0) count++;
     }
 
     return count;
 }
 
-int[] testCase = {345,897,568,234};
+int[] testCase = { 345, 897, 568, 234 };
 int testAnswer = 2;
 
+Console.WriteLine($"[{string.Join(",", testCase)}] -> {GetCountOfEvenNumbers(testCase)}");
+
 bool CheckingSolution(int[] checkingArray, int answer)
-{   
-    Console.WriteLine(string.Join(",", checkingArray));
+{
+
     return (GetCountOfEvenNumbers(checkingArray) == answer) ? true : false;
 }
 
-
-Console.WriteLine(CheckingSolution(testCase,testAnswer));
+Console.WriteLine(CheckingSolution(testCase, testAnswer));
