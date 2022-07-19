040decec0b0aded8a0363dfd5525f5e2bdc834ec
renat-amiryanov <rfamiryanov@gmail.com>
2022-07-19 10:06:14 +0300

    Практическая работа. Семинар 6. Задача 41.

---
+++ Seminar006-Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+    Задача 41. Пользователь вводит с клавиатуры M чисел.
+    Посчитать, сколько чисел больше 0 ввел пользователь.
+*/
+
+int[] CreateAndFillArray(int m)
+{
+    int[] numbers = new int[m];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = int.Parse(Console.ReadLine());
+    }
+
+    return numbers;
+}
+int GetCountOfPositiveNumbers(int[] numbers, int i = 0)
+{
+    int count = 0;
+    if (numbers[i] > 0) count++;
+    if (numbers.Length - 1 != i)
+    {
+        return count + GetCountOfPositiveNumbers(numbers, i + 1);
+    }
+    else
+    {
+        return count;
+    }
+}
+
+int[] inputArray = CreateAndFillArray(5);
+Console.WriteLine($"[{string.Join(",", inputArray)}] -> {GetCountOfPositiveNumbers(inputArray)}");
+
