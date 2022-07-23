b2dc36706f8bb111c4880280f9ece0877ff35383
renat-amiryanov <rfamiryanov@gmail.com>
2022-07-23 12:30:07 +0300

    Практическое занятие. Семинар 7. Задача 52.

--- Seminar007-Task52/Program.cs
+++ Seminar007-Task52/Program.cs
@@ -7,15 +7,17 @@
     8 4 2 4
 
     Среднее арифметическое каждого столбца:4,6;5,6;3,6;3.
+
+
+    1 4 7 2     a[0,0]+a[1,0]+a[2,0] = 1+5+8 = 14  
+    5 9 2 3     a[0,1]+a[1,1]+a[2,1] = 4+9+4 = 17
+    8 4 2 4     a[0,2]+a[1,2]+a[2,2] = 7+2+2 = 11
+                a[0,3]+a[1,3]+a[2,3] = 2+3+4 = 9
 */
 
-int[,] array2D = {
-    {1,4,7,2},
-    {5,9,2,3},
-    {8,4,2,4}
-};
 
-void Display(int[,] values)
+
+void Display2D(int[,] values)
 {
     int row = values.GetLength(0);
     int col = values.GetLength(1);
@@ -30,5 +32,32 @@ void Display(int[,] values)
     }
 
 }
+void GetSumCalcAvgOfColumnsValueAndDisplay(int[,] arr2D)
+{
+        
+    int row = arr2D.GetLength(0);
+    int col = arr2D.GetLength(1);
+    double[] result = new double[col];
 
-Display(array2D);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            result[j] += arr2D[i, j];
+        }
+    }
+    for (int i = 0; i <  result.Length; i++) 
+    {
+        Console.Write($"{(result[i]/row):0.00}; ");
+    }  
+    Console.WriteLine(); 
+}
+
+int[,] array2D = {
+    {1,4,7,2},
+    {5,9,2,3},
+    {8,4,2,4}
+};
+Display2D(array2D);
+System.Console.Write("Среднее арифметическое каждого столбца: ");
+GetSumCalcAvgOfColumnsValueAndDisplay(array2D);
