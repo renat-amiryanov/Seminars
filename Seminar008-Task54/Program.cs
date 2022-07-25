f6b44de10b61c9500cf4c9b3ea3844d3b7734cd6
renat-amiryanov <rfamiryanov@gmail.com>
2022-07-25 07:40:54 +0300

    Initial commit

---
+++ Seminar008-Task54/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+В итоге получается вот такой массив:
+
+1 2 4 7
+2 3 5 9
+2 4 4 8
+
+*/
+
+
+
+void Display(int[,] values)
+{
+    int row = values.GetLength(0);
+    int col = values.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            Console.Write($"{values[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] SortingRows(int[,] values)
+{
+    int row = values.GetLength(0);
+    int col = values.GetLength(1);
+    int[,] sortedRows = new int[row, col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = col - 1; j > 0; j--)
+        {
+            for (int k = 0; k < j; k++)
+            {
+                if (values[i, k] > values[i, k + 1])
+                {
+                    int temp = values[i, k];
+                    values[i, k] = values[i, k + 1];
+                    values[i, k + 1] = temp;
+                }
+
+            }
+
+        }
+    }
+    return values;
+}
+
+int[,] input = {
+    {1,4,7,2},
+    {5,9,2,3},
+    {8,4,2,4}
+};
+Display(input);
+Console.WriteLine();
+int[,] result = SortingRows(input);
+Display(result);
---
+++ Seminar008-Task56/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2  a[0,0]+a[0,1]+a[0,2]+a[0,3] = 1+4+7+2 = 14 
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+*/
+int[] GetSumOfArrayRow(int[,] values)
+{
+
+    int row = values.GetLength(0);
+    int col = values.GetLength(1);
+    int[] returnArray = new int[values.GetLength(0)];
+    for (int i = 0; i < row; i++)
+    {
+
+        for (int j = 0; j < col; j++)
+        {
+            returnArray[i] += values[i, j];
+
+        }
+
+    }
+    return returnArray;
+}
+
+
+int[,] array2D = {
+    {1,4,7,2},
+    {5,9,2,3},
+    {8,4,2,4},
+    {5,2,6,7}
+};
+
+int[] sumArrayRowsElements = GetSumOfArrayRow(array2D);
+System.Console.WriteLine(String.Join(",", sumArrayRowsElements));
