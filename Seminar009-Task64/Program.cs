a0c62e1ce66309eb77603fcc6a42311d2a27a870
renat-amiryanov <rfamiryanov@gmail.com>
2022-07-26 18:26:34 +0300

    Initial commit

---
+++ Seminar009-Task64/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+    Задача 66. Задайте значения M и N. Напишите программу которая выведет все натуральные числа от M до N
+    M = 1; N =5.->"1,2,3,4,5"
+*/
+
+string RealNumbers(int M, int N)
+{
+    return M == N ? N.ToString() : M.ToString() + " " + RealNumbers(M += 1, N);
+}
+
+int M = 1;
+int N = 5;
+System.Console.WriteLine(RealNumbers(M, N));
