323e11d943fc49969219e8dabcd59a1778743804
renat-amiryanov <rfamiryanov@gmail.com>
2022-07-26 18:24:38 +0300

    Практическая работа. Семинар 8. Задача 60.

--- Seminar008-Task60/Program.cs
+++ Seminar008-Task60/Program.cs
@@ -10,3 +10,70 @@
 45(1,0,0) 53(1,0,1)
 
 */
+
+
+int[] UniqueArray(int size)
+{
+    int[] a = new int[size];
+    for (int i = 0; i < a.Length; i++)
+    {
+        bool isPresent;
+        do
+        {
+            a[i] = new Random().Next(10, 100);
+            isPresent = false;
+            for (int j = 0; j < i; j++)
+            {
+                int nextNumber = new Random().Next(10, 100);
+                if (a[j] == nextNumber)
+                {
+                    isPresent = true;
+                    break;
+                }
+
+            }
+        } while (isPresent);
+    }
+    return a;
+}
+
+int[,,] Generate3DArray(int n)
+{
+    int[,,] array3D = new int[n, n, n];
+    int[] uniqueNumbers = UniqueArray(array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2));   
+   
+    int counter = 0;
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++, counter++)
+            {
+                array3D[i, j, k] = uniqueNumbers[counter];
+            }
+        }
+
+    }
+    return array3D;
+
+}
+void Display3DArray(int[,,] data)
+{
+    for (int i = 0; i < data.GetLength(0); i++)
+    {
+        for (int j = 0; j < data.GetLength(1); j++)
+        {
+            for (int k = 0; k < data.GetLength(2); k++)
+            {
+                System.Console.Write($"{data[i, j, k]}({i},{j},{k}) ");
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,,] result = Generate3DArray(2);
+
+Display3DArray(result);
+
