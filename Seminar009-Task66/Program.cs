6c107c73cf63b56a8f3188909b006f837ba2e413
renat-amiryanov <rfamiryanov@gmail.com>
2022-07-27 09:41:28 +0300

    Практическая работа. Семинар 9. Задача 66ю

---
+++ Seminar009-Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+    Задача 66.
+     Задайте значения M и N. Напишите программу которая выведет сумму натуральных элементов в промежутке от M до N
+    M = 1; N = 15.-> 120
+    M = 4; N = 8.-> 120
+*/
+
+int GetSumOfRealNumbers(int m, int n)
+{
+    return m == n ?  n: m += GetSumOfRealNumbers(m+1, n);
+}
+
+int m = 1;
+int n = 15;
+int sumOfRealNumbers = GetSumOfRealNumbers(m,n);
+Console.WriteLine($"M = {m};N = {n} -> {sumOfRealNumbers}");
+
+m = 4;
+n = 8;
+sumOfRealNumbers = GetSumOfRealNumbers(m,n);
+Console.WriteLine($"M = {m};N = {n} -> {sumOfRealNumbers}");
